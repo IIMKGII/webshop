5b5f41811b7595b56b64f24932f2d613c5b65f20
IIMKGII <waigmann.thorsten@gmail.com>
2022-04-07 23:06:19 +0200

    finished product

--- src/Exercises/Product.php
+++ src/Exercises/Product.php
@@ -44,6 +44,28 @@ final class Product
      * @return void Returns nothing
      */
 
+    private function initDB(): void
+    {
+        $charsetAttr="SET NAMES utf8 COLLATE utf8_general_ci";
+        $dsn="mysql:host=db;port=3306;dbname=onlineshop";
+        $mysqlUser="onlineshop";
+        $mysqlPwd="geheim";
+        $multi=false;
+        $options = array(
+            // A warning is given for persistent connections in case of a interrupted database connection.
+            // This warning is shown on the web page if error_reporting=E_ALL is set in php.ini
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            // PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+            // PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_NUM,
+            // PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_BOTH,
+            PDO::MYSQL_ATTR_INIT_COMMAND => $charsetAttr,
+            PDO::MYSQL_ATTR_MULTI_STATEMENTS => $multi
+        );
+        $this->dbh = new PDO($dsn, $mysqlUser, $mysqlPwd, $options);
+    }
+
     /**
      * Validates the user input
      *
@@ -62,17 +84,45 @@ final class Product
      */
     public function isValid(): void
     {
+        if (Utilities::isEmptyString($_POST['product_name'])){
+            $this->messages['product_name'] = "Please enter a product.";
+        }
+        if (Utilities::isEmptyString($_POST['price'])){
+            $this->messages['price'] = "Please enter a price.";
+        }
+        if (!Utilities::isInt($_POST['price'])){
+            $this->messages['price'] = "Please enter a valid price.";
+        }
+        if (isset($_POST['active'])){
+            $this->messages['active'] = "Please select active/inactive";
+        }
+        if (Utilities::isEmptyString($_POST['short_description'])){
+            $this->messages['short_description'] = "Please enter a short description.";
+        }
+        if (Utilities::isEmptyString($_POST['long_description'])){
+            $this->messages['long_description'] = "Please enter a long description.";
+        }
+        if(!Utilities::isEmptyString($_POST['product_name']) && !$this->isUniqueProductName()) {
+            $this->messages['product_name'] = "This product already exists";
+        }
+        if(!Utilities::isEmptyString($_POST['product_category_name']) && !$this->isValidCategory()) {
+            $this->messages['product_category_name'] = "Please select a valid category";
+        }
         if ((count($this->messages) === 0)) {
             $this->business();
         } else {
-            $this->twigParams['product_name']= $_POST['product_name'];
-            $this->twigParams['price']= $_POST['price'];
-            $this->twigParams['short_description']= $_POST['short_description'];
-            $this->twigParams['long_description']= $_POST['long_description'];
-            $this->twigParams['selected']= Utilities::sanitizeFilter($_POST['product_category_name']);
-            $this->twigParams['active']= $_POST['active'];
+            $this->twigParams['product_name'] = $_POST['product_name'];
+            $this->twigParams['price'] = $_POST['price'];
+            $this->twigParams['short_description'] = $_POST['short_description'];
+            $this->twigParams['long_description'] = $_POST['long_description'];
+            $this->twigParams['selected'] = Utilities::sanitizeFilter($_POST['product_category_name']);
+
+            if (isset($_POST['active'])) {
+            $this->twigParams['active'] = $_POST['active'];
+        }
+
             $this->twigParams['messages']= $this->messages;
-            $this->twigParams['productCategory']= $this->fillProductCategory();;
+            $this->twigParams['productCategory']= $this->fillProductCategory();
             $this->twig->display("product.html.twig", $this->twigParams);
         }
     }
@@ -85,6 +135,7 @@ final class Product
      */
     protected function business(): void
     {
+        $this->addProduct();
         $this->messages['status'] = "Your product has been added successfully";
         $this->twigParams['messages']= $this->messages;
         $this->twigParams['productCategory']= $this->fillProductCategory();;
@@ -108,6 +159,7 @@ SQL;
             $this->stmt->execute();
             $result = $this->stmt->fetchAll();
         }
+
         return $result;
     }
 
@@ -118,7 +170,18 @@ SQL;
      */
     private function isValidCategory(): bool
     {
-            return true;
+        $query = /** @lang MySQL */
+            <<<SQL
+                SELECT product_category_name from product_category
+                where product_category_name = :product_category_name;
+SQL;
+        if ($this->dbh) {
+            $this->stmt = $this->dbh->prepare($query);
+            $this->stmt->bindValue(':product_category_name' , $_POST['product_category_name'], PDO::PARAM_STR);
+            $this->stmt->execute();
+            $result = $this->stmt->fetchAll();
+        }
+        return (count($result))===0;
     }
 
     /**
@@ -128,7 +191,18 @@ SQL;
      */
     private function isUniqueProductName(): bool
     {
-            return true;
+        $query = /** @lang MySQL */
+            <<<SQL
+                SELECT product_name from product
+                where product_name = :product_name;
+SQL;
+        if ($this->dbh) {
+            $this->stmt = $this->dbh->prepare($query);
+            $this->stmt->bindValue(':product_name' , $_POST['product_name'], PDO::PARAM_STR);
+            $this->stmt->execute();
+            $result = $this->stmt->fetchAll();
+        }
+        return (count($result))===0;
     }
 
     /**
@@ -138,5 +212,27 @@ SQL;
      */
     private function addProduct(): void
     {
+        $query = /** @lang MySQL */
+            <<<SQL
+                 INSERT INTO  product
+                 SET product_name = :product_name,
+                     product_category_name = :product_category_name,
+                     price = :price,
+                     short_description = :short_description,
+                     long_description = :long_description,
+                     active = :active,
+                     date_added = :date_added
+        SQL;
+        if ($this->dbh) {
+            $this->stmt = $this->dbh->prepare($query);
+            $params = [":product_name" => $_POST['product_name'],
+                ":product_category_name" => $_POST['product_category_name'],
+                ":price" => $_POST['price'],
+                ":short_description" => $_POST['short_description'],
+                ":long_description" => $_POST['long_description'],
+                ":active" => $_POST['active'],
+                ":date_added" => date('Y-m-d H:i:s')];
+            $this->stmt->execute($params);
+        }
     }
 }
